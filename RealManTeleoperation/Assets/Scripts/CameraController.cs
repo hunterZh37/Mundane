d43533aa46baa9bda2ef875063865e564b3ba671
hunterZh37 <hunterzhang121@outlook.com>
2025-09-21 15:51:03 -0700

    Added ROS-TCP-Connector

---
+++ RealManTeleoperation/Assets/Scripts/CameraController.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        this.transform.position = new Vector3(0, 0.5f, -1.75f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
